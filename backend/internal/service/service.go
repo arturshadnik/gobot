cc4829da2a5d205f8d651c5c09e093b1a2b1efa8
Artur Shad Nik <arturshadnik@gmail.com>
2024-02-28 23:30:52 -0800

    Update API response model to match UI.

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -3,33 +3,38 @@
 package service
 
 import (
+	"time"
+
 	"github.com/arturshadnik/gobot/backend/internal/config"
 	"github.com/arturshadnik/gobot/backend/internal/db"
+	"github.com/arturshadnik/gobot/backend/internal/models"
 	"github.com/arturshadnik/gobot/backend/pkg/utils"
 )
 
-func ProcessIncomingMsg(message string, level string, id string) (string, error) {
+func ProcessIncomingMsg(message string, level string, id string) (models.ConvoMessage, error) {
 	err := db.StoreMessage(message, level, "user", id)
 	if err != nil {
-		return "", err
+		return models.ConvoMessage{}, err
 	}
 
 	conversation, err := db.LoadConversation(id, level)
 	if err != nil {
-		return "", err
+		return models.ConvoMessage{}, err
 	}
 
 	messages, err := db.GetMessages(conversation.Messages)
 	if err != nil {
-		return "", err
+		return models.ConvoMessage{}, err
 	}
-
+	for _, msg := range messages {
+		delete(msg, "Timestamp")
+	}
 	convConfig, err := config.GetLevelSettings(level)
 	if err != nil {
-		return "", err
+		return models.ConvoMessage{}, err
 	}
 
-	sysMsg := []map[string]string{{
+	sysMsg := []map[string]any{{
 		"role":    "system",
 		"content": convConfig.Prompt,
 	}}
@@ -44,13 +49,13 @@ func ProcessIncomingMsg(message string, level string, id string) (string, error)
 
 	resp, err := utils.GetOpenAIResponse(openaiCompletionConfig)
 	if err != nil {
-		return "", err
+		return models.ConvoMessage{}, err
 	}
 
 	err = db.StoreMessage(resp, level, "assistant", id)
 	if err != nil {
-		return "", nil
+		return models.ConvoMessage{}, nil
 	}
 
-	return resp, nil
+	return models.ConvoMessage{Role: "assistant", Content: resp, Timestamp: time.Now()}, nil
 }
